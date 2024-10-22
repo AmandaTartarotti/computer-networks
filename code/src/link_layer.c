39eb34b3f2267316d109b878913a7cac668ae3a3
fojacob <up202102359@up.pt>
2024-10-22 14:56:17 +0100

    llwrite meio confusa

--- code/src/link_layer.c
+++ code/src/link_layer.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 
 // MISC
@@ -24,9 +25,13 @@
 #define C_REJ1 0x55
 #define C_DISC 0x0B
 
+#define ESC 0x70
+
 int alarmEnabled = FALSE;
 int alarmCount = 0;
+unsigned char frameTx = 0;
 unsigned char byte;
+int totalRetransmitions, timeout;
 
 void alarmHandler(int signal)
 {   
@@ -64,7 +69,8 @@ int llopen(LinkLayer connectionParameters)
         return -1;
     }
     state current_state = START;
-
+    totalRetransmitions = connectionParameters.nRetransmissions;
+    timeout = connectionParameters.timeout;
     switch (connectionParameters.role)
     {
     case LlTx: //Transmitter
@@ -72,11 +78,11 @@ int llopen(LinkLayer connectionParameters)
         (void)signal(SIGALRM, alarmHandler);
         alarmCount = 0;
 
-        while (alarmCount < connectionParameters.nRetransmissions && current_state != STOP_RCV) {
+        while (alarmCount < totalRetransmitions && current_state != STOP_RCV) {
             sendControlFrame(A_T, C_SET);
             printf("Transmission: %d\n", alarmCount+1);
 
-            alarm(connectionParameters.timeout);
+            alarm(timeout);
             alarmEnabled = TRUE;
 
             while(current_state != STOP_RCV && alarmEnabled==TRUE) {
@@ -119,7 +125,7 @@ int llopen(LinkLayer connectionParameters)
                     }
                 }
             }
-    }
+        }
         break;
     case LlRx:
         while (current_state != STOP_RCV) {
@@ -177,9 +183,106 @@ int llopen(LinkLayer connectionParameters)
 ////////////////////////////////////////////////
 int llwrite(const unsigned char *buf, int bufSize)
 {
-    // TODO
+    int framesize = bufSize+6;
+    state current_state = START;
+    alarmCount = 0;
+    unsigned char cField = 0;
+    unsigned char *frame = (unsigned char *) malloc(framesize);
+    frame[0] = FLAG;
+    frame[1] = A_T;
+    frame[2] = frameTx ? 0x80 : 0x00;
+    frame[3] = frame[1] ^ frame[2];
 
-    return 0;
+    unsigned char BCC2 = buf[0];
+    for (int i = 0; i < bufSize; i++) BCC2 ^= buf[i];
+
+    memcpy(frame+4, buf, bufSize);
+
+    int j = 4;
+
+    for (int i = 0; i < bufSize; i++) {
+        if(buf[i] == FLAG || buf[i] == ESC) {
+            framesize++;
+            frame = realloc(frame, framesize);
+            frame[j] = ESC;
+            j++;
+        }
+        frame[j] = buf[i];
+        j++;
+    }
+
+    frame[j] = BCC2;
+    j++;
+    frame[j] = FLAG;
+    j++;
+
+    while (alarmCount < totalRetransmitions && current_state != STOP_RCV) {
+            writeBytesSerialPort(frame, j);
+            printf("Transmission: %d\n", alarmCount+1);
+
+            alarm(timeout);
+            alarmEnabled = TRUE;
+
+            while(current_state != STOP_RCV && alarmEnabled==TRUE) {
+                if(readByteSerialPort(&byte)) {
+                    printf("buffer -- 0x%02X\n", byte);
+                    switch (current_state)
+                    {
+                    case START:
+                        if (byte == FLAG) current_state = FLAG_RCV;
+                        break;
+
+                    case FLAG_RCV:
+                        if (byte == A_R) current_state = A_RCV;
+                        else if (byte == FLAG) break;
+                        else current_state = START;
+                        break;
+
+                    case A_RCV:
+                        if (byte == FLAG) current_state = FLAG_RCV;
+                        else if (byte == C_RR0 || byte == C_RR1 || byte==C_REJ0 || byte==C_REJ1){
+                            current_state = C_RCV;
+                            cField = byte;
+                        }
+                        else current_state = START;
+                        break;
+
+                    case C_RCV:
+                        if (byte == FLAG) current_state = FLAG_RCV;
+                        else if (byte == (A_R^C_UA)) current_state = BCC1_OK;
+                        else current_state = START;
+                        break;
+
+                    case BCC1_OK:
+                        if (byte == FLAG){
+                            current_state = STOP_RCV;
+                        }
+                        else 
+                            current_state = START;
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
+
+            // if(!cField){
+            //     continue;
+            // }
+
+            // elseif(cField == C_REJ0 || cField == C_REJ1){ A execução ja para no momento em que o state é STOP_RCV
+            //     rejected = 1;
+            // }
+
+            if(cField == C_RR0 || cField == C_RR1){
+                free(frame);
+                frameTx = (frameTx+1) % 2;
+                return framesize;
+            }
+        }
+    free(frame);
+    llclose(1);
+    return -1;
 }
 
 ////////////////////////////////////////////////
